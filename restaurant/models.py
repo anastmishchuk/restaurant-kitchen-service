9bc1ce5c30c4b374a20ca676f3279c76d6068e79
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2024-02-24 16:23:59 +0100

    Updated Dish & Cook classes: optional description, added get_absolute_url

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator, MaxValueValidator
 
@@ -27,10 +28,13 @@ class Cook(AbstractUser):
     def __str__(self):
         return f"{self.username} ({self.first_name} {self.last_name})"
 
+    def get_absolute_url(self):
+        return reverse("restaurant:cook-detail", kwargs={"pk": self.pk})
+
 
 class Dish(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
     cooks = models.ManyToManyField(Cook, related_name="cooks")
@@ -41,3 +45,6 @@ class Dish(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("restaurant:dish-detail", kwargs={"pk": self.pk})
