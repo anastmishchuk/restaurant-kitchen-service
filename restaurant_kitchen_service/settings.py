bdb4af657346b55e83c69b7395c5f78c1c32c25d
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2024-02-28 15:21:33 +0100

    setting up for implementing styles

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -65,6 +65,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "restaurant.context_processors.cfg_assets_root",
             ],
         },
     },
@@ -130,6 +131,7 @@ STATICFILES_DIRS = (BASE_DIR / "static",)
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+ASSETS_ROOT = "/static/assets"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
